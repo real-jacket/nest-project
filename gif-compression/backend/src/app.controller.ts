1d3b8f19c5cecff1090d070a3c743fcd2ef8cd32
real-jacket <1762982273@qq.com>
2023-07-23 17:17:37 +0800

    feat: finish compress

--- gif-compression/backend/src/app.controller.ts
+++ gif-compression/backend/src/app.controller.ts
@@ -1,5 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+  Res,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { existsSync } from 'fs';
+import { Response } from 'express';
+import * as sharp from 'sharp';
 
 @Controller()
 export class AppController {
@@ -8,5 +22,41 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Post('upload')
+  @UseInterceptors(
+    FileInterceptor('file', {
+      dest: 'uploads',
+    }),
+  )
+  uploadFile(@UploadedFile() file: Express.Multer.File) {
+    console.log('file: ', file);
+
+    return file.path;
+  }
+
+  @Get('compress')
+  async compression(
+    @Query('path') filePath: string,
+    @Query('color', ParseIntPipe) color: number,
+    @Query('level', ParseIntPipe) level: number,
+    @Res() res: Response,
+  ) {
+    if (!existsSync(filePath)) {
+      throw new BadRequestException('文件不存在');
+    }
+
+    const data = await sharp(filePath)
+      .png({
+        compressionLevel: level,
+        force: true,
+        palette: true,
+      })
+      .toBuffer();
+
+    res.set('Content-Disposition', `attachment; filename="dest.png"`);
+
+    res.send(data);
   }
 }
--- gif-compression/frontend/src/App.tsx
+++ gif-compression/frontend/src/App.tsx
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Upload, UploadProps, message, Form, Input, Button } from 'antd';
+import { InboxOutlined } from '@ant-design/icons';
+import axios from 'axios';
+
+const { Dragger } = Upload;
 
 function App() {
+  const [form] = Form.useForm();
+  const [filePath, setFilePath] = useState('');
+  const [fileName, setFileName] = useState('');
+
+  const compress = async (values: any) => {
+    const res = await axios.get('http://localhost:3005/compress', {
+      params: {
+        color: values.color || 256,
+        level: values.level || 9,
+        path: filePath,
+      },
+      responseType: 'arraybuffer',
+    });
+
+    const blog = new Blob([res.data], { type: 'image/png' });
+    const url = URL.createObjectURL(blog);
+    const link = document.createElement('a');
+    link.href = url;
+    link.download = fileName;
+    link.click();
+
+    message.success('压缩成功');
+  };
+
+  const props: UploadProps = {
+    name: 'file',
+    action: 'http://localhost:3005/upload',
+    maxCount: 1,
+    onChange(info) {
+      const { status } = info.file;
+
+      if (status === 'done') {
+        message.success(`${info.file.name} 文件上传成功`);
+
+        setFileName(info.file.name);
+        setFilePath(info.file.response);
+      } else if (status === 'error') {
+        message.error(`${info.file.name} 文件上传失败`);
+      }
+    },
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Form
+        style={{ width: 500, margin: '50px auto' }}
+        form={form}
+        onFinish={compress}
+      >
+        <Form.Item label="颜色数量" name={'color'}>
+          <Input />
+        </Form.Item>
+        <Form.Item label="压缩级别" name="level">
+          <Input />
+        </Form.Item>
+        <Form.Item>
+          <Dragger {...props}>
+            <div>
+              <p className="ant-upload-drag-icon">
+                <InboxOutlined />
+              </p>
+              <p className="ant-upload-text">点击或拖拽文件到这个区域来上传</p>
+            </div>
+          </Dragger>
+        </Form.Item>
+        <Form.Item>
+          <Button type="primary" htmlType="submit">
+            压缩
+          </Button>
+        </Form.Item>
+      </Form>
     </div>
   );
 }
